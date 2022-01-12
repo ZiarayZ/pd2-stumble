92f5e1db19e04f85f43419811cc9c969bb6a5ba4
Jake <31741096+ZiarayZ@users.noreply.github.com>
2022-01-12 14:55:26 +0000

    Add files via upload

---
+++ Stumble/trip.lua
@@ -0,0 +1,15 @@
+PlayerStandard = PlayerStandard or class(PlayerMovementState)
+--percentage chance to stumble/trip from 0 to 100, i suggest below 0.25%
+local trip_chance = (0.25 / 100)
+
+Hooks:PostHook(PlayerStandard, "_update_movement", "UpdateMovement", function(self, t, dt)
+    if self._running and math.random() <= trip_chance then
+        if not self._state_data.ducking then
+            self:_start_action_ducking(t)
+        end
+        self._ext_damage:damage_simple({
+            variant = "delayed_tick",
+            damage = self._ext_damage:get_real_armor()
+        })
+    end
+end)
